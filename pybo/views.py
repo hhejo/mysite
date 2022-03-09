e92e30452bdf0e9c55b23d4e17dd04d5990e44cd
hhejo <litsmainjof@gmail.com>
2022-03-09 22:42:09 +0900

    220309

---
+++ pybo/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question, Answer
+from .forms import QuestionForm, AnswerForm
+# from django.http import HttpResponse
+
+
+def index(request):
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+    # return HttpResponse('pybo')
+
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    """
+    pybo 답변 등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if 'POST' == request.method:
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
+
+
+def question_create(request):
+    """
+    pybo 질문 등록
+    """
+    if 'POST' == request.method:
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
